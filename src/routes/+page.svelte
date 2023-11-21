0b51bfddd8dd13ddf2a09dc6ffea35349a850ad2
lucaoudejans <luca.oudejans@hva.nl>
2023-11-21 12:00:53 +0100

    begin project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,49 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+	<link rel="stylesheet" href="style.css" />
+</svelte:head>
+
+<script>
+	import CharacterSelector from '../components/personData.svelte';
+
+  let selectedCharacter = null;
+
+  function handleCharacterClick(character) {
+    selectedCharacter = character;
+  }
+</script>
+
+<main>
+	<h1>Choose your charachter</h1>
+
+		<ul>
+			<li><img src="images/harry.png" alt="harry potter"></li>
+			<li><img src="images/hermelien.png" alt="harry potter"></li>
+			<li><img src="images/ron.png" alt="harry potter"></li>
+			<li><img src="images/neville.png" alt="harry potter"></li>
+			<li><img src="images/draco.png" alt="harry potter"></li>
+			<li><img src="images/sirius.png" alt="harry potter"></li>
+			<li><img src="images/sneep.png" alt="harry potter"></li>
+		</ul>
+</main>
+
+<style>
+	h1 {
+		text-align: center;
+		font-family:Verdana, Geneva, Tahoma, sans-serif;
+	}
+
+	ul {
+		list-style: none;
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		padding: 5em;
+		cursor: pointer;
+	}
+
+	img {
+		width: 7em;
+	}
+</style>
+
+<CharacterSelector />
