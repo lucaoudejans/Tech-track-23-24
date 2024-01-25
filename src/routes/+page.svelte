f076756fc0b15e57376b6646ae5b3367f1d0a589
lucaoudejans <luca.oudejans@hva.nl>
2024-01-25 21:58:57 +0100

    fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,8 +8,8 @@ The resources can be found in my Wiki! -->
 <!-- importing onmount and components -->
 <script>
 import { onMount } from 'svelte';
-import Barchart from '../barchart.svelte';
-import Dotplot from '../components/dotplot.svelte';
+import Barchart from '../components/barchart.svelte';
+import Dotplot from '../components/Dotplot.svelte';
 
 
   let characterData = {};
